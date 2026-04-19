c7f5025f57866fb3316881fce0efc783a2048f37
SEH00N <06psh0501@gmail.com>
2026-04-19 14:57:20 +0900

    [Level] Set Height collider

---
+++ ShootingHero/Assets/01.Scripts/Runtime/Shared/ETC/StairCollider.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace ShootingHero.Shared
+{
+    public class StairCollider : MonoBehaviour
+    {
+        [SerializeField]
+        private int height = 0;
+
+        public void OnTriggerEnter2D(Collider2D collider)
+        {
+            if(collider.TryGetComponent<Unit>(out Unit unit) == false)
+                return;
+            
+            unit.SetHeight(height);
+        }
+    }
+}
